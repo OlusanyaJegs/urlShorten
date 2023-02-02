6ba98c90f0d1536304b5229fdb7048b47345c771
OlusanyaJegs <olusanya@backdrop.photo>
2023-02-02 12:38:43 +0000

    chore: made adjustments to the mutation to check if the url is valid, create a short code and also return the original url

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { buildSchema } = require("graphql");
 const validUrl = require('valid-url');
 const PORT = 4000;
 
+// const urlExists = import('url-exists-nodejs');
+
 const { Database } = require('fakebase');
 const db = new Database('./data');
 const Url = db.table('urls')
@@ -22,7 +24,8 @@ const schema = buildSchema(`
     }
 
     type Mutation {
-        shortenUrl(url: String!): String
+        shortenUrl(url: String!): String,
+        originalUrl(id: String!): String
     }
     
 `);
@@ -46,9 +49,28 @@ const urlMap = {};
 
 const rootValue = {
     shortenUrl: ({ url }) => {
+
+        if (!validUrl.isUri(url)) {
+            throw new Error("This URL is Invalid.");
+        }
+
         const id = (Math.random() + 1).toString(36).substring(6);
         urlMap[id] = url;
+
         return `http://localhost:4000/graphql/${id}`;
+    },
+
+    // this takes the id as an argument and returns the original Url associated with it, if it exists.
+    originalUrl: ({ id }) => {
+        const url = urlMap[id];
+
+        // check if it exists
+        if (!url) {
+            throw new Error("This URL is Invalid.");
+        }
+
+        // returns the original Url
+        return url;
     },
 
     getUrls: () => Url.findAll(),
@@ -62,12 +84,18 @@ app.use("/graphql", graphqlHTTP({
     graphiql: true,
 }));
 
+
 app.get("/:shortUrl", (req, res) => {
     const { id } = req.params;
     const url = urlMap[id];
 
     // check if url to be shortened is valid
-    if (validUrl.isUri(url)) {
+    // if (urlExists) {
+    //     res.redirect(url);
+    // } else {
+    //     res.send("This URL is Invalid.");
+    // }
+    if (url) {
         res.redirect(url);
     } else {
         res.send("This URL is Invalid.");
